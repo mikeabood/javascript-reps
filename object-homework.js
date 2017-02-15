2a6a94c809d3668b806114b437f43cd96ad8f18b
Michael Abood <Abood@Mikes-Macbook.local>
2017-02-15 01:11:52 -0600

    completed reps hw

---
+++ object-homework.js
@@ -0,0 +1,26 @@
+var words = ["Hey", "whats", "up", "hello"];
+var lengthOfArray = [];
+
+function = lengths(arrStrings){
+
+for(i=0; i < words.length; i++){
+ lengthOfArray.push(words[i].length);
+}
+	return lengthOfArray;
+}
+
+lengths(arrStrings)
+
+//ROUND 2
+
+
+
+function transmogrifier(x,y,z){
+	var q = (x * y);
+	return math.pow(q, z);
+}
+
+transmogrifier(5, 4, 3);
+transmogrifier(13, 12, 5);
+transmogrifier(42, 13, 7);
+
